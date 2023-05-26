da8e4f2908e6b86ce94b364fb66db4204e4aaaef
Maria Nadal <nadal.miro@gmail.com>
2023-05-26 12:21:36 +0200

    Añadida configuración inicial para BBDD Definición de ruta general api

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
+const apiRouter = require("./routes/api");
 
 app.use(cors);
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.use("/api", apiRouter);
 
 module.exports = app;
