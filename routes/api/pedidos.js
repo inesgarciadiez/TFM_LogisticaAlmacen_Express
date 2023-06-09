4f4ec48f64a7a0449cc19efd42e9d2c96c678b52
Maria Nadal <nadal.miro@gmail.com>
2023-06-09 11:10:55 +0200

    TFM-LA-5: VER DETALLE PEDIDO

--- routes/api/pedidos.js
+++ routes/api/pedidos.js
@@ -1,19 +1,54 @@
-const { getAllByEstadosYUsuario, updateState, getAllClosedStateAndUser } = require("../../models/pedido.model");
-const { checkOperario } = require("../../utils/middlewares.js");
-const { HttpError } = require("../../utils/errores");
+const {
+  getAllByEstadosYUsuario,
+  getById,
+  updateState, 
+  getAllClosedStateAndUser
+} = require('../../models/pedido.model');
+const { checkOperario } = require('../../utils/middlewares.js');
+const { HttpError } = require('../../utils/errores');
+const { getAlmacenByNombre } = require('../../models/almacen.model');
 
-const router = require("express").Router();
+const router = require('express').Router();
 
 const estadosOperario = [
-  "NUEVO",
-  "ERROR",
-  "LISTO_ENTRADA",
-  "LISTO_SALIDA",
-  "CERRADO",
+  'NUEVO',
+  'ERROR',
+  'LISTO_ENTRADA',
+  'LISTO_SALIDA',
+  'CERRADO',
 ];
 
+const checkPedidoPermisos = async (pedido, usuarioId) => {
+  // Si el estado del pedido es uno de los estados de operario y el responsable(operario) no es el usuario que hace la petición --> error.
+  if (
+    estadosOperario.includes(pedido.estado) &&
+    pedido.responsable_id != usuarioId
+  ) {
+    return 'El pedido no está a cargo del usuario solicitante (OPERARIO)';
+  }
+
+  // Si el estado del pedido es PTE_SALIDA, busco el responsable(encargado) del almacen_oriden del pedido. Si no es igual al usuario que hace la petición --> error.
+  if (pedido.estado === 'PTE_SALIDA') {
+    const [almacenOrigen] = await getAlmacenByNombre(pedido.almacen_origen);
+    console.log(almacenOrigen);
+    if (almacenOrigen[0].responsable_id != usuarioId) {
+      return 'El pedido no está a cargo del usuario solicitante (ENCARGADO ORIGEN)';
+    }
+  }
+
+  // Si el estado del pedido es PTE_ENTRADA, busco el responsable(encargado) del almacen_destino del pedido. Si no es igual al usuario que hace la petición --> error.
+  if (pedido.estado === 'PTE_ENTRADA') {
+    const [almacenDestino] = await getAlmacenByNombre(pedido.almacen_destino);
+    if (almacenDestino[0].responsable_id != usuarioId) {
+      return 'El pedido no está a cargo del usuario solicitante (ENCARGADO DESTINO)';
+    }
+  }
+
+  return null;
+};
+
 // GET /api/pedidos/operario
-router.get("/operario", checkOperario, async (req, res) => {
+router.get('/operario', checkOperario, async (req, res) => {
   const usuarioId = req.user.id;
   try {
     const [result] = await getAllByEstadosYUsuario(estadosOperario, usuarioId);
@@ -56,6 +91,36 @@ router.put("/operario/:referencia/:estado", checkOperario, async (req, res) => {
     );
     return res.status(errorMetodo.codigoEstado).json(errorMetodo);
   }
+});
+
+// GET /api/pedidos/pedidoId
+router.get('/:pedidoId', async (req, res) => {
+  const { pedidoId } = req.params;
+
+  let pedido;
+
+  try {
+    const [pedidoById] = await getById(pedidoId);
+    if (pedidoById.length === 0) {
+      const error = new HttpError('No existe el pedido con ese Id', 404);
+      return res.status(error.codigoEstado).json(error);
+    }
+    pedido = pedidoById[0];
+  } catch (error) {
+    const errorMetodo = new HttpError(
+      `Error en el acceso de recuperar pedido: ${error.message}`,
+      422
+    );
+    return res.status(errorMetodo.codigoEstado).json(errorMetodo);
+  }
+
+  let mensajeError = await checkPedidoPermisos(pedido, req.user.id);
+  if (mensajeError) {
+    const error = new HttpError(mensajeError, 403);
+    return res.status(error.codigoEstado).json(error);
+  }
+
+  res.json(pedido);
 });
 
 module.exports = router;
