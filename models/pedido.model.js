162f5d5bc1511011cb6d0c87e7695d7edc5a7aae
Diego <diegochilet99@gmail.com>
2023-06-17 16:24:45 +0200

    cambiando datos de los pedidos en el front

--- models/pedido.model.js
+++ models/pedido.model.js
@@ -14,7 +14,6 @@ const getAllByEstadosYUsuario = (estadosOperario, usuarioId) => {
 const getAllPedidosByEncargado = (usuarioId) => {
   return db.query(`SELECT pe.id as referencia, estado_id, fecha_creacion, fecha_salida, matricula, detalles, alo.nombre as almacen_origen, ald.nombre as almacen_destino, es.estado FROM pedidos AS pe
   INNER JOIN almacenes AS alo ON pe.almacen_origen_id = alo.id
-  INNER JOIN almacenes AS alm ON pe.responsable_id = alm.id
   INNER JOIN almacenes AS ald ON pe.almacen_destino_id = ald.id
   INNER JOIN estados AS es ON pe.estado_id = es.id
   WHERE estado = 'PTE_SALIDA' 
